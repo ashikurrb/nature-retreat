dbf30b7f9fe869398868a2864365dc26a8ea729b
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2026-04-08 12:04:39 +0600

    remove unwanted files

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "./_sections/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/herobg-sm.png')] md:bg-[url('/herobg3.png')] bg-center md:bg-top bg-cover min-h-screen max-w-360 mx-auto">
+    <div className="bg-[url('/herobg-sm.png')] md:bg-[url('/herobg-lg.png')] bg-center md:bg-top bg-cover min-h-screen max-w-360 mx-auto">
       <Header />
       <Hero />
     </div>
